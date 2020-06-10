953902cc223eba8acdfd7cd9c14e5d8787cf653c
Winston Van <winston.van7@gmail.com>
2020-06-10 15:25:03 -0500

    Serialize

--- train.py
+++ train.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 import os
 import chess.pgn
+from state import State
+
 for fn in os.listdir("data"):
     while True:
         pgn = open(os.path.join("data", fn))
         game = chess.pgn.read_game(pgn)
-        print(game.headers)
+        result = game.headers["Result"]
         board = game.board()
+        value = {"1/2-1/2": 0, "0-1": -1, "1-0": 1}
         for i, move in enumerate(game.mainline_moves()):
             board.push(move)
-            print(i, board)
+        print(value[result], State(board).serialize())
         if game is None:
             break
     exit(0)
-
