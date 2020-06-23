838d4166141e9de93ac80696cd91e06557cb25ea
Winston Van <winston.van7@gmail.com>
2020-06-23 13:03:03 -0500

    Classic valuator

--- play.py
+++ play.py
@@ -18,6 +18,88 @@ def explore_leaves(s, v):
 
 s = State()
 
+class ClassicValuator:
+    values = {chess.PAWN: 1,
+            chess.KNIGHT: 3,
+            chess.BISHOP: 3,
+            chess.ROOK: 5,
+            chess.QUEEN: 9,
+            chess.KING: 0}
+
+    def __init__(self):
+        self.reset()
+        self.memo = {}
+
+    def reset(self):
+        self.count = 0
+
+    def __call__(self, s):
+        self.count += 1
+        key = s.key()
+        if key not in self.memo:
+            self.memo[key] = self.value(s)
+        return self.memo[key]
+
+    def value(self, s):
+        b = s.board
+        if b.is_game_over():
+            if b.result() == "1-0":
+                return MAXVAL
+            elif b.result() == "0-1":
+                return -MAXVAL
+            else:
+                return 0
+        val = 0.0
+        # Material
+        pm = s.board.piece_map()
+        for x in pm:
+            tval = self.values[pm[x].piece_type]
+            if pm[x].color == chess.WHITE:
+                val += tval
+            else:
+                val -= tval
+        # Legal moves
+        bak = b.turn
+        b.turn = chess.WHITE
+        val += 0.1 * b.legal_moves.count()
+        b.turn = chess.BLACK
+        val -= 0.1 * b.legal_moves.count()
+        b.turn = bak
+        return val
+
+def computer_minimax(s, v, depth, a, b):
+    if depth >= 5 or s.board.is_game_over():
+        return v(s)
+    if s.board.turn == chess.WHITE:
+        ret = -MAXVAL
+    else:
+        ret = MAXVAL
+    isort = []
+    for e in s.board.legal_moves:
+        s.board.push(e)
+        isort.append((v(s), e))
+        s.board.pop()
+    move = sorted(isort, key=lambda x: x[0], reverse=s.board.turn) # WHITE == True
+    # Beam search
+    if depth >= 3:
+        move = move[:10]
+
+    for e in [x[1] for x in move]:
+        s.board.push(e)
+        tval = computer_minimax(s, v, depth+1, a, b)
+        s.board.pop()
+        if s.board.turn == chess.WHITE:
+            ret = max(ret, tval)
+            a = max(a, ret)
+            if a >= b: # b cut-off
+                break
+        else:
+            ret = min(ret, tval)
+            b = min(b, ret)
+            if a >= b: # a cut-off
+                break
+    return ret
+
 @app.route("/")
 def hello():
     ret = open("index.html").read()
